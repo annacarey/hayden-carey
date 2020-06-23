f71119e13f536d882f6311803b9440f0e40db4a1
github username <annacarey@icloud.com>
2020-06-23 10:47:41 -0600

    added mickey cursor

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,13 @@ module.exports = {
         test: /\.css$/,
         loaders: ['style-loader', 'css-loader'],
         include: `${__dirname}/src`
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
       }
     ]
   },
