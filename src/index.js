a0550e93987588559e1adc7e86b3472d689a4c36
github username <annacarey@icloud.com>
2020-06-23 09:47:24 -0600

    added hover feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/main.css'
 
+window.addEventListener('DOMContentLoaded', (event) => {
+  const header = document.getElementById('header')
+
+  header.addEventListener('mouseover', e => {
+    const selectedMenuItem = document.getElementById(e.target.id)
+    console.log(selectedMenuItem)
+    if (selectedMenuItem.id === 'about-me-option') {
+      document.getElementById(e.target.id.replace('-option', '')).style.display = 'block'
+      console.log(selectedMenuItem)
+    }
+  })
+})
+
 console.log('Hello Webpack Project.')
